6dec023579e464cde9516dee2f153373580e45ff
franklin <franklinhpf@hotmail.com>
2022-07-01 00:45:44 -0300

    nova opcao

--- conexao.php
+++ conexao.php
@@ -1,9 +1,9 @@
 <?php
-define('HOST', '185.201.11.23');
+define('HOST', '195.179.237.204');
 define('USUARIO', 'u334305405_iR2gX');
 define('SENHA', 'Cordeiro1998@');
 define('DB', 'u334305405_Dq5IC');
 
-$pdo = new PDO('mysql:host=185.201.11.23;dbname=u334305405_Dq5IC', 'u334305405_iR2gX', 'Cordeiro1998@', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+$pdo = new PDO('mysql:host=195.179.237.204;dbname=u334305405_Dq5IC', 'u334305405_iR2gX', 'Cordeiro1998@', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 
 
--- data.php
+++ data.php
@@ -1,12 +1,12 @@
 <?php
 include('./conexao.php');
 
-// $query = "SELECT d.nome, d.cpf, d.plano, d.preco, d.status, d.data , p.beneficiarios  FROM `dadospessoais` AS d INNER JOIN `dadosprincipais` AS p ON d.forekey = p.forekey WHERE p.beneficiarios <> '' AND d.nome <> '' ORDER BY d.id DESC";
-// $sql = $pdo->prepare($query, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
-// $sql->execute();
-// $results = $sql->fetchAll(PDO::FETCH_ASSOC);
-// $json_data['data'] = $results;
-// print_r(json_encode($json_data));
+ $query = "SELECT d.nome, d.cpf, d.plano, d.preco, d.status, d.data , p.beneficiarios  FROM `dadospessoais` AS d INNER JOIN `dadosprincipais` AS p ON d.forekey = p.forekey WHERE p.beneficiarios <> '' AND d.nome <> '' ORDER BY d.id DESC";
+ $sql = $pdo->prepare($query, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+ $sql->execute();
+ $results = $sql->fetchAll(PDO::FETCH_ASSOC);
+ $json_data['data'] = $results;
+ print_r(json_encode($json_data));
 
 // $query = "SELECT * FROM `dadospessoais` AS d WHERE vendedor = 'ti@unidentis.com.br'";
 // $sql = $pdo->prepare($query, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
